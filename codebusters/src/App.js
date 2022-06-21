74d2d7c4dec37c4ee1a3e83eb01a340caeb2629f
soting <sotingip@gmail.com>
2022-06-21 10:51:14 +0100

    useState and useEffect to fetch from PostGres so far

--- codebusters/src/App.js
+++ codebusters/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {useState, useEffect} from "react";
 import logo from './logo.svg';
 import './App.css';
 import ToggleSwitch from './ToggleSwitch.js';
@@ -9,6 +10,20 @@ import Buttons from './Buttons.js';
 
 
 function App() {
+  const [data, setData]=useState([])
+
+useEffect(()=> {
+  async function errorMsg() {
+    //this needs the local host url from the backend server
+    const response = await fetch("https://    ")
+    const dataFetch = await response.json();
+    setData(dataFetch.data);
+  }
+  errorMsg();
+}, [])
+
+
+
 
   return (
     <><div className="App">
