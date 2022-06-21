99cbfe6c05bf5c0ebeb34557e1f41b8df41f4e33
Osztian42 <103217445+Osztian42@users.noreply.github.com>
2022-06-21 10:35:41 +0100

    cssdone

---
+++ codebusters/src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+import ToggleSwitch from './ToggleSwitch.js';
+import index from './index.js';
+import Buttons from './Buttons.js';
+
+
+
+
+function App() {
+
+  return (
+    <><div className="App">
+      
+      <header className="App-header">
+        <div className = "groupTitle">
+          </div>
+      </header>
+      
+      
+      
+          </div> <img src={logo} className="App-logo" alt="logo" />
+
+      <>
+       <ToggleSwitch label="Dark mode" />
+       <Buttons />
+
+
+
+
+
+
+       </>
+       
+        
+     
+    </>);
+};
+
+export default App;
---
+++ codebusters/src/Buttons.js
@@ -0,0 +1,67 @@
+import React from "react";
+import "./Buttons.css";
+
+
+
+const Buttons = ({ label }) => {
+    return (
+<div class='wrapper'>
+
+
+
+
+  <div class='bttn out cyan'>
+    <span>VS Code</span>
+    <div class='corners top'></div>
+    <div class='corners bottom'></div>
+  </div>
+
+
+
+
+  <div class='bttn in'>
+    <span>NPM</span>
+    <div class='corners top'></div>
+    <div class='corners bottom'></div>
+  </div>
+
+
+
+
+  <div class='bttn out cyan'>
+    <span>GitHub</span>
+    <div class='corners top'></div>
+    <div class='corners bottom'></div>
+  </div>
+
+
+
+
+{/*
+ <div class='box out'>
+    <div class='content'>
+      <p>Bacon ipsum dolor amet meatloaf corned beef frankfurter tongue</p>
+    </div>
+    <div class='corners top'></div>
+    <div class='corners bottom'></div>
+  </div>
+
+
+//MORE BUTTONS IF NEEDED
+
+  <div class='box in'>
+    <div class='content'>
+      <p>Short ribs boudin cow landjaeger spare ribs cupim.</p>
+    </div>
+    <div class='corners top'></div>
+    <div class='corners bottom'></div>
+  </div>
+    
+    */}
+
+
+</div>
+    )}
+
+
+export default Buttons;
