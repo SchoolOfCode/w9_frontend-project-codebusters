c0dd0f14d9d9ea90838182335a35847d4bc20aad
Out-A-Time <101604946+Out-A-Time@users.noreply.github.com>
2022-06-22 15:23:24 +0100

    Question and answer displayed poorly

--- codebusters/src/App.js
+++ codebusters/src/App.js
@@ -1,61 +1,59 @@
 import React from "react";
-import {useState} from "react";
+import { useState } from "react";
 // import logo from './logo.svg';
-import logo from './food.jpg';
-import './App.css';
-import ToggleSwitch from './ToggleSwitch.js';
-import index from './index.js';
-import Button from './Button.js';
+import logo from "./food.jpg";
+import "./App.css";
+import ToggleSwitch from "./ToggleSwitch.js";
+import index from "./index.js";
+import Button from "./Button.js";
 import Databox from "./Databox.js";
 
-
 function App() {
-  const [data, setData]=useState([])
+  const [data, setData] = useState([]);
 
   async function npmErrorMsg(topicType) {
     //this needs the local host url from the backend server
-    const response = await fetch(`http://localhost:3001/movies/${topicType}`)
+    const response = await fetch(`http://localhost:3001/movies/${topicType}`);
     const dataFetch = await response.json();
     setData(dataFetch.payload);
-    console.log(data)
+    console.log(data);
   }
 
-
-const payload = data.map(payload =>
+  const payload = data.map((payload) => (
+    <div>
       <div>{payload.question}</div>
-      )
-      console.log(payload)
-
+      <div>{payload.answer}</div>
+    </div>
+  ));
+  console.log(payload);
 
   return (
-    
-    <><div className="App">
-      
-      <header className="App-header">
-        <div className = "groupTitle">
-          </div>
-      </header>
-      
-      
-      
-          {/* </div> <img src={logo} className="App-logo" alt="logo" /> */}
-          </div> <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <div className="App">
+        <header className="App-header">
+          <div className="groupTitle"></div>
+        </header>
+
+        {/* </div> <img src={logo} className="App-logo" alt="logo" /> */}
+      </div>{" "}
+      <img src={logo} className="App-logo" alt="logo" />
       <>
-       <ToggleSwitch label="Dark mode" />
+        <ToggleSwitch label="Dark mode" />
 
-        <div className="drinksdata">
-        {payload}
+        <div className="drinksdata">{payload}</div>
+
+        <div className="wrapper">
+          <Button label="VS CODE" topicType="VS" onClick={npmErrorMsg} />
+          <Button
+            label="NPM"
+            topicType="npm"
+            styleType="in"
+            onClick={npmErrorMsg}
+          />
+          <Button label="GITHUB" topicType="Git" onClick={npmErrorMsg} />
         </div>
-
-        <div className ='wrapper'>
-       <Button  label="VS CODE" topicType= "VS" onClick={npmErrorMsg} />
-       <Button  label="NPM" topicType="npm" styleType="in" onClick={npmErrorMsg} />
-       <Button  label="GITHUB" topicType="Git" onClick={npmErrorMsg} />
-       </div>
-       </>
-       
-        
-     
-    </>);
-};
+      </>
+    </>
+  );
+}
 export default App;
