ba48ac01014f0fa7445c3aa6188016c924b00d08
uklawson <uklawson@gmail.com>
2022-06-29 11:03:26 +0100

    changed indentation button.js

--- codebusters/src/Button/Button.js
+++ codebusters/src/Button/Button.js
@@ -4,17 +4,14 @@ const Button = ({ label, topicType, styleType, onClick }) => {
 
   const buttonClasses = (styleType === 'in') ? "bttn in" : "bttn out cyan"
 
-      return (
-  <>
-    <div onClick={()=>onClick(topicType)} className={buttonClasses}>
-      <span>{label}</span>
-      <div className='corners top'></div>
-      <div className='corners bottom'></div>
-    </div>
-  </>
-
-      )}
-
-      
+  return (
+    <>
+      <div onClick={()=>onClick(topicType)} className={buttonClasses}>
+        <span>{label}</span>
+        <div className='corners top'></div>
+        <div className='corners bottom'></div>
+      </div>
+    </>
+  )}
 
 export default Button;
